703152e5e8a6d8aa2dd132ba746319df7b2b7eb8
bhupathiraju vinay <vinayvarma.bhr@gmail.com>
2020-10-15 13:17:06 +0530

    firstcommit

---
+++ wordcount/views.py
@@ -0,0 +1,31 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def sample(request):
+    return render(request,'home.html')
+def message(request):
+    return HttpResponse("message")
+
+def content(request):
+    return render(request,'about.html')
+
+
+def cnt(request):
+    text=request.GET['fulltext']
+    wordlist= text.split()
+    print(len(wordlist))
+    dup=[]
+    for word in wordlist:
+        print(word,wordlist.count(word))
+        if wordlist.count(word)>1:
+            dup.append(word)
+    print(dup)
+
+
+
+
+
+
+
+    return render(request,'count.html',{'tst': text,'wordlist':wordlist,'count':len(wordlist),'duplicates':dup})
